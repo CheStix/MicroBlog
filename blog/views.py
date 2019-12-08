8c0de5e4785526eb8d740c431d34594697a9eeff
Che.El <che.eldar@gmail.com>
2019-12-08 22:07:53 +0300

    added  in PostView def post

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from django.views.generic import View
 
 from .models import Post
@@ -8,6 +9,16 @@ from .forms import PostForm
 class PostView(View):
     """Сообщения пользователя"""
     def get(self, request):
-        posts = Post.objects.all()
+        posts = Post.objects.order_by('-id')
         form = PostForm()
         return render(request, 'blog/index.html', {'posts': posts, 'form': form})
+
+    def post(self, request):
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.user = request.user
+            form.save()
+            return redirect('posts_view_url')
+        else:
+            return HttpResponse('error')
